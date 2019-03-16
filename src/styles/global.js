04030ff6d6d5b14954fcf12b506ba6ec8c973015
Piia <piia.hartikka@helsinki.fi>
2019-03-16 18:23:29 +0200

    global ul

--- src/styles/global.js
+++ src/styles/global.js
@@ -4,6 +4,10 @@ const GlobalStyle = createGlobalStyle`
     body {
         background-color: ${props => props.theme.colors.outerSpace};
     }
+
+    ul {
+        list-style-type: none;
+    }
 `;
 
 export default GlobalStyle;
