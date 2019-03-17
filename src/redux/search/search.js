dbfb7cc151d973ddb26793ef639885a60959029c
Piia <piia.hartikka@helsinki.fi>
2019-03-17 20:33:13 +0200

    Added parameter and error state to redux/search

--- src/redux/search/search.js
+++ src/redux/search/search.js
@@ -4,20 +4,28 @@ import SpotifyClient from 'spotify_client/SpotifyClient';
 const initialState = {
     tracks: [],
     loading: false,
+    error: {}
 }
 
 
 const SEARCH_TRACKS = 'search/SEARCH_TRACKS';
 const SEARCH_TRACKS_SUCCESS = 'search/SEARCH_TRACKS_SUCCESS';
 const SEARCH_TRACKS_FAIL = 'search/SEARCH_TRACKS_FAIL';
-export const searchTracks = () => {
+export const searchTracks = trackName => {
     return async dispatch => {
         dispatch({
             type: SEARCH_TRACKS,
         });
 
+        if (!trackName) {
+            return dispatch({
+                type: SEARCH_TRACKS_FAIL,
+                error: { message: 'No trackName' },
+            });
+        }
+
         try {
-            const result = await SpotifyClient.searchTracks();
+            const result = await SpotifyClient.searchTracks(trackName);
             return dispatch({
                 type: SEARCH_TRACKS_SUCCESS,
                 result: result.data,
@@ -44,5 +52,6 @@ export const searchReducer = (state = initialState, action) => ({
     [SEARCH_TRACKS_FAIL]: ({
         ...state,
         loading: false,
+        error: action.error,
     }),
 })[action.type] || state;
