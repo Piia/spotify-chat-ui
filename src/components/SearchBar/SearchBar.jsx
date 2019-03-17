738ff144ae432d4178599c66b8763a9a08888e3d
Piia <piia.hartikka@helsinki.fi>
2019-03-17 22:25:29 +0200

    SearchBar with label and input

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import SearchInput from 'components/Inputs/SearchInput';
 
+const Bar = styled.div`
+    padding: ${props => props.theme.spacing.md} 0;
+`;
+Bar.displayName = 'Bar';
 
-const SearchBar = props => {
-    return <div>Search</div>
+const Label = styled.label`
+    display: block;
+    padding: ${props => props.theme.spacing.xs} ${props => props.theme.spacing.md};
+    font-family: ${props => props.theme.font.family.verdana};
+    font-size: ${props => props.theme.font.size.sm};
+    font-weight: ${props => props.theme.font.weight.normal};
+    color: ${props => props.theme.colors.varden};
+`;
+Label.displayName = 'Label';
+
+class SearchBar extends PureComponent {
+    state = {
+        model: {}
+    };
+
+    handleInputChange = (property, value) => {
+        this.setState(oldState => ({ model: { ...oldState.model, [property]: value } }))
+    };
+
+    render() {
+        return (
+            <Bar>
+                <Label>Search tracks</Label>
+                <SearchInput
+                    onChange={ this.handleInputChange }
+                    property="trackName"
+                    model={ this.state.model }
+                />
+            </Bar>
+        );
+    };
 };
 
 SearchBar.propTypes = {
--- src/components/SearchBar/SearchBar.spec.js
+++ src/components/SearchBar/SearchBar.spec.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import SearchInput from 'components/Inputs/SearchInput';
 
 describe('SearchBar', () => {
     let component;
@@ -9,5 +10,34 @@ describe('SearchBar', () => {
 
     it('should render component', () => {
         expect(component).toExist();
+    });
+
+    it('should render label', () => {
+        expect(component.find('Label')).toExist();
+    });
+
+    it('should render input', () => {
+        expect(component.find(SearchInput)).toExist();
+    });
+
+    it('should pass props to input', () => {
+        const passedProps = component.find(SearchInput).props();
+        expect(passedProps.onChange).toBeDefined();
+        expect(passedProps.property).toBeDefined();
+        expect(passedProps.model).toBeDefined();
+    });
+
+    describe('when onChange is called', () => {
+        let property, value;
+
+        beforeEach(() => {
+            property = 'asdf';
+            value = 'ghjk';
+            component.find(SearchInput).props().onChange(property, value);
+        });
+
+        it('should change state', () => {
+            expect(component.state().model[property]).toEqual(value);
+        });
     });
 });
