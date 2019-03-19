7ec6f27b4ed8a908d9e062a644f62186e21696df
Piia <piia.hartikka@helsinki.fi>
2019-03-19 20:23:22 +0200

    Added sinon to dev-dependencies, added more globals to setupTests and .eslintrc

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,9 +1,12 @@
 import { configure, shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'jest-enzyme';
+import { spy, stub } from 'sinon';
 
 configure({ adapter: new Adapter() });
 
 global.shallow = shallow;
 global.render = render;
 global.mount = mount;
+global.spy = spy;
+global.stub = stub;
