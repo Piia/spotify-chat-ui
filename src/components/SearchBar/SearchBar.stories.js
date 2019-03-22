8101468b6478d8f78207de6cbfbdd8d18b4c35fb
Piia <piia.hartikka@helsinki.fi>
2019-03-22 23:39:09 +0200

    Added green background to SearhBar story

--- src/components/SearchBar/SearchBar.stories.js
+++ src/components/SearchBar/SearchBar.stories.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import SearchBar from './SearchBar';
+import { theme } from 'styles/theme';
 
 storiesOf('SearchBar', module)
-    .add('default', () => (
-        <SearchBar onSearch={ action('search') } />
+    .add('default', props => (
+        <div style={{ padding: '3em', backgroundColor: theme.colors.goblin }}>
+            <SearchBar onSearch={ action('search') } />
+        </div>
     )); 
