3f5e9eebdc700ec7ae3235b2bdecce20d19feb68
Piia <piia.hartikka@helsinki.fi>
2019-04-09 21:23:45 +0300

    PlayButton refactored

--- src/components/PlayButton/PlayButton.jsx
+++ src/components/PlayButton/PlayButton.jsx
@@ -1,36 +1,10 @@
 import React, { PureComponent } from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { resume, pause } from 'redux/playback/playback';
 
 
-const basicButtonStyles = css`
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-    box-sizing: content-box;
-    width: 3.5rem;
-    height: 3.5rem;
-    padding: 0;
-    background-color: ${props => props.theme.colors.cosmic};
-    border-radius: 50%;
-    border: 10px solid ${props => props.theme.colors.black};
-    color: ${props => props.theme.colors.black};
-    text-align: center;
-    font-family: ${props => props.theme.font.family.arial};
-    font-size: 2rem;
-    transition: background-color 0.2s ease-in;;
-
-    &:hover {
-        cursor: pointer;
-        background-color: ${props => props.theme.colors.magnolia};
-    }
-    &:focus {
-        outline: none;
-    }
-`;
-
-export const Button = styled.button`
+const Button = styled.button`
     display: inline-flex;
     align-items: center;
     justify-content: center;
@@ -76,17 +50,37 @@ class PlayButton extends PureComponent {
         play: false,
     };
 
+    componentDidMount() {
+        this.resetPlay();
+    };
+
+    componentDidUpdate(prevProps) {
+        if (this.props.isPlaying !== prevProps.isPlaying) {
+            this.resetPlay();
+        }
+    };
+
+    resetPlay = () => {
+        this.setState({ play: this.props.isPlaying });
+    };
+
+    togglePlay = () => {
+        this.setState(oldState => ({ play: !oldState.play }));
+    };
+
     handlePause = event => {
         event.preventDefault();
+        this.togglePlay();
         this.props.pause();
     };
     handleResume = event => {
         event.preventDefault();
+        this.togglePlay();
         this.props.resume();
     };
 
     render() {
-        const isPlaying = this.props.isPlaying;
+        const isPlaying = this.state.play;
 
         return (
             <Button onClick={ isPlaying ? this.handlePause : this.handleResume }>
