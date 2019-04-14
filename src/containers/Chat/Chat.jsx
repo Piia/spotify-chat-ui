170f4afc019c4248e8c80e55aa64cfc210ff927b
Jiri Hartikka <jiri.hamberg@gmail.com>
2019-04-14 20:09:29 +0300

    Chat styles

--- src/containers/Chat/Chat.jsx
+++ src/containers/Chat/Chat.jsx
@@ -6,10 +6,14 @@ import withChatClient from 'components/withChatClient/withChatClient';
 
 
 const MessageContainer = styled.article`
-    padding: 0 ${props => props.theme.spacing.xl};
+    margin-left: ${props => props.theme.spacing.xl};
+    padding: ${props => props.theme.spacing.xs}; 
     overflow-x: hidden;
     overflow-y: scroll;
-    height: calc(100% - 6rem);
+    height: calc(100% - 7rem);
+    background-color: ${props => props.theme.colors.outerSpace};
+    box-sizing: border-box;
+    border: 1px solid ${props => props.theme.colors.black};
 `;
 MessageContainer.displayName = 'Container';
 
@@ -17,6 +21,7 @@ const ChatSection = styled.section`
     height: calc(100vh - 130px - 50px);
     max-height: calc(100vh - 130px - 50px);
     min-height: calc(100vh - 130px - 50px);
+    background-color: ${props => props.theme.colors.voodoo};
 `;
 ChatSection.displayName = 'ChatSection';
 
@@ -25,25 +30,48 @@ const InputTextArea = styled.textarea`
     height: 5rem;
     max-height: 5rem;
     min-height: 5rem; 
-    width: 75%;
-    max-width: 75%;
-    min-width: 75%;
     resize: none;
-    padding: 0;
+    padding: ${props => props.theme.spacing.xs};
     box-sizing: border-box;
+    background-color: ${props => props.theme.colors.outerSpace};
+    border: none;
+    color: ${props => props.theme.colors.magnolia};
+    width: 100%;
+    border: 1px solid ${props => props.theme.colors.black};
+
+    &:focus {
+        outline: none;
+    }
 `;
 InputTextArea.displayName = "InputTextArea";
 
 const SubmitButton = styled.button`
     height: 5rem;
     width: 7rem;
+    background-color: ${props => props.theme.colors.outerSpace};
+    border: 1px solid ${props => props.theme.colors.black};
+    border-left-width: 0;
+    color: ${props => props.theme.colors.varden};
+    font-family: ${props => props.theme.font.family.verdana};
+    font-size: ${props => props.theme.font.size.sm};
+    font-weight: ${props => props.theme.font.weight.bold};
+    font-stretch: normal;
+    text-transform: uppercase;
+
+    &:hover {
+        color: ${props => props.theme.colors.leather};
+        cursor: pointer;
+    }
+
 `;
 SubmitButton.displayName = "SubmitButton";
 
 const Container = styled.section`
-    height: 6rem;
+    padding-right: ${props => props.theme.spacing.xl};
+    height: 7rem;
     display: flex;
     align-items: center;
+    margin-left: ${props => props.theme.spacing.xl};
 `;
 Container.displayName = "Container";
 
