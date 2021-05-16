36c7e1dc7e503807ea9096461a74ddd1e3131b55
Jiri Hartikka <jiri.hartikka@gmail.com>
2021-05-16 12:23:20 +0300

    Image to ts

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { FC } from 'react';
 import styled from 'styled-components';
-import PropTypes from 'prop-types';
 
 const StyledImage = styled.img`
     width: ${props => props.width}px;
@@ -12,30 +11,29 @@ const StyledImage = styled.img`
 `;
 StyledImage.displayName = 'StyledImage';
 
-const Placeholder = styled.div`
+type PlaceHolderProps = {
+    width: number;
+    height: number;
+};
+
+const Placeholder = styled.div<PlaceHolderProps>`
     width: ${props => props.width}px;
     height: ${props => props.height}px;
     background-color: ${props => props.theme.colors.leather};
 `;
 Placeholder.displayName = 'Placeholder';
 
-const Image = ({ url, width, height }) => {
+export type ImageProps = {
+    url?: string;
+    width?: number;
+    height?: number;
+};
+
+const Image: FC<ImageProps> = ({ url, width = 64, height = 64 }) => {
     if (!url) {
         return <Placeholder width={width} height={height} />;
     }
     return <StyledImage src={url} width={width} height={height} />;
-};
-
-Image.defaultProps = {
-    url: null,
-    width: 64,
-    height: 64,
-};
-
-Image.propTypes = {
-    url: PropTypes.string,
-    width: PropTypes.number,
-    height: PropTypes.number,
 };
 
 export default Image;
